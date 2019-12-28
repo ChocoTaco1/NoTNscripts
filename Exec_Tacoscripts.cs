fb753be73c35aca631acb3fdbe0790a3d5cab53a
ChocoTaco <boldfilter@zoho.com>
2019-12-28 11:14:20 -0500

    Create Exec_Tacoscripts.cs

---
+++ Exec_Tacoscripts.cs
@@ -0,0 +1,30 @@
+// Linux does not load files in order like windows
+// This causes weird things to happen.
+// Things dont get loaded when they are suppose to.
+// This midigates that.
+
+// Execute Scripts in order
+exec("scripts/tacoscripts/AllowBotSkin.cs");
+exec("scripts/tacoscripts/AntiCloak.cs");
+exec("scripts/tacoscripts/AntiTurret.cs");
+exec("scripts/tacoscripts/Autobalance.cs");
+exec("scripts/tacoscripts/ExploitPatches.cs");
+exec("scripts/tacoscripts/GetRandomMaps.cs");
+exec("scripts/tacoscripts/GetTeamCounts.cs");
+exec("scripts/tacoscripts/IPAdminList.cs");
+exec("scripts/tacoscripts/MapRepetitionChecker.cs");
+exec("scripts/tacoscripts/MinivstationX.cs");
+exec("scripts/tacoscripts/MissionTypeOptions.cs");
+exec("scripts/tacoscripts/NoBaseRapeNotify.cs");
+exec("scripts/tacoscripts/ObserverCooldown.cs");
+exec("scripts/tacoscripts/TacoOverrides.cs");
+exec("scripts/tacoscripts/TeamBalanceNotify.cs");
+exec("scripts/tacoscripts/VehiclePassengerEject.cs");
+exec("scripts/tacoscripts/VoteOverTime.cs");
+exec("scripts/tacoscripts/VoteSound.cs");
+exec("scripts/tacoscripts/zAltAltHeartbeatDaemon.cs");
+exec("scripts/tacoscripts/zDarkTigerAFK.cs");
+exec("scripts/tacoscripts/zDarkTigerStats.cs");
+exec("scripts/tacoscripts/zDebriefLoadingScreen.cs");
+exec("scripts/tacoscripts/zSmurfNameColorFix.cs");
+exec("scripts/tacoscripts/GetGUID.cs");
