732d7b8ac8e9a5358a57fa179af0bfb5788d263a
ChocoTaco <boldfilter@zoho.com>
2020-01-01 01:02:40 -0500

    Update

---
+++ scripts/autoexec/zAltConnectLog.cs
@@ -0,0 +1,49 @@
+//All other connect logging is off
+$Host::ClassicConnectLog = 0;
+$Host::EvoConnectLogging = 0;
+
+package AltConnectLog
+{
+	
+function GameConnection::onConnect(%client, %name, %raceGender, %skin, %voice, %voicePitch)
+{
+	parent::GameConnection::onConnect(%client, %name, %raceGender, %skin, %voice, %voicePitch);
+	
+	// Log the connection
+	Alt_connectLog(%client);
+}
+	
+};
+
+// Prevent package from being activated if it is already
+if (!isActivePackage(AltConnectLog))
+    activatePackage(AltConnectLog);
+
+// connectLog(%client, %realname, %tag)
+// Info: Logs the connections
+function Alt_connectLog(%client)
+{
+   if($Host::EvoConnectLogging)
+   {
+      // get the client info
+      %authInfo = %client.getAuthInfo();
+	
+	  // connect info
+	  %ConnectLogPlayerCount = $HostGamePlayerCount;
+	  $ConnectLog = formatTimeString("d-M-yy") SPC formatTimeString("[HH:nn]") SPC %client.nameBase SPC "(" @ getField(%authInfo, 0) @ "," SPC %client.guid @ "," SPC %client.getAddress() @ ")" SPC "Pop[" @ %ConnectLogPlayerCount @ "]" SPC "Map[" @ $CurrentMission @ "]";
+
+      // log the message
+      if($Host::EvoDailyLogs)
+      {
+         if(formatTimeString("HH") > getSubStr($Host::EvoDailyHour, 0, strstr($Host::EvoDailyHour, ":")) || (formatTimeString("HH") == getSubStr($Host::EvoDailyHour, 0, strstr($Host::EvoDailyHour, ":")) && formatTimeString("nn") >= getSubStr($Host::EvoDailyHour, strstr($Host::EvoDailyHour, ":")+1, 2)))
+            export("$ConnectLog", "logs/Connect/ConnectLog-" @ formatTimeString("d-M-yy") @ ".txt", true);
+         else
+         {
+            %yesterday = formatTimeString("d") - 1;
+            export("$ConnectLog", "logs/Connect/ConnectLog-" @ %yesterday @ formatTimeString("-M-yy") @ ".txt", true);
+         }
+      }
+      else
+         export("$ConnectLog", "logs/Connect/ConnectLog.txt", true);
+   }
+}
